b568898c54a139a5a9cf4a028c46fdf168ec751c
hujalex <ahuwindsor@gmail.com>
2024-12-19 17:22:06 -0500

    reformating

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ from typing import Union
 app = FastAPI()
 load_dotenv()
 
+#Open data
+with open('./data/handbook.json', 'r') as file:
+    handbook_data = json.load(file)
+with open('./data/tutorresources.json', 'r') as file:
+    tutor_resources_data = json.load(file);
+
+client = AsyncGroq(
+    api_key=os.getenv("GROQ_API_KEY"),
+)
 
 
 class Item(BaseModel):
@@ -26,17 +35,6 @@ async def read_item(message: Item):
     except Exception as e:
         print("Read Item Exception")
         raise HTTPException(status_code = 500, detail = f"Error in processing query {e}")
-    
-
-#Open data
-with open('./data/handbook.json', 'r') as file:
-    handbook_data = json.load(file)
-with open('./data/tutorresources.json', 'r') as file:
-    tutor_resources_data = json.load(file);
-
-client = AsyncGroq(
-    api_key=os.getenv("GROQ_API_KEY"),
-)
 
 
 # @cached(ttl = 3600)
@@ -54,7 +52,7 @@ async def call_Groq(query : str) -> str:
                 },
                 {
                     "role":"system",
-                    "content" : "Always provide the CONNECT_ME_HANDBOOK link if necessary to question"
+                    "content" : "Always provide the CONNECT_ME_HANDBOOK link if necessary to answer the prompt"
                 },
                 {
                     "role": "system",
@@ -73,31 +71,3 @@ async def call_Groq(query : str) -> str:
         raise HTTPException(status_code = 500, detail = f"Error in Groq call {e}")
         # return "Please wait for a moment before sending another prompt"
     
-
-
-
-# for line in sys.stdin:
-
-#     chat_completion = client.chat.completions.create(
-#         messages=[
-#             {
-#                 "role": "system",
-#                 "content": f"{handbook_data} {tutor_resources_data} Your are a helpful assistant answering questions based off the data given",
-#             },
-#             {
-#                 "role": "system",
-#                 "content": f"Provide links as much as possible",
-#             },
-#             {
-#                 "role":"system",
-#                 "content" : "Always provide the CONNECT_ME_HANDBOOK link if necessary to question"
-#             },
-#             {
-#                 "role": "user",
-#                 "content": f"{line}",
-#             },
-#         ],
-#         model="llama3-70b-8192",
-#     )
-
-#     print(chat_completion.choices[0].message.content)
